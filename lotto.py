3aa247a22ce54d4dfd07888230fb414760a01e0e
Mike Wyatt <uranusdemilo@gmail.com>
2019-01-29 13:49:25 -0800

    New Files

--- lotto.py
+++ lotto.py
@@ -1,19 +1,59 @@
-class lottocol(object):
-	def __init__(self,columnNum,numBalls):
-		self.columnNum = columnNum
-		self.numBalls = numBalls
-		
-	def printnum(self):
-		sys.stdout.write('Column Number: ')
-		print self.columnNum
-import sys		
-infile = open("lottosorted.txt","r")
-line = 'junk'
-while line:
-	line = infile.readline()
-	sys.stdout.write(line)
-	
-col1 = lottocol(1,40)
+class matrix:
+    def __init__(self):
+        self.col=[0]
+        self.col.append(col(47))
+        self.col.append(col(47))
+        self.col.append(col(47))
+        self.col.append(col(47))
+        self.col.append(col(47))
+        self.col.append(col(27))
+        self.firstDraw=1375
+        self.lastDraw=0
+class col:
+    def __init__(self,numBalls):
+        self.numBalls=numBalls
+        self.ball=[0]
+        self.leastHits=1000
+        self.mostHits=0
+        self.leastCommon=0
+        self.mostCommon=0
+        self.sortedHits=[]
+        ###Instantiate Balls in Row
+        for x in range(1,numBalls + 1):
+            self.ball.append(lottoBall(x))
+class lottoBall:
+    def __init__(self, number):
+        self.number=number
+        self.hits=0
+        self.score=0
+        self.hitMatrix=[]
+        self.diffMatrix=[]
+        self.meanDist=0
+        self.sdevDist=0 #Standard Deviation for Distance Vector
+        self.diffPull=0
+        self.lastHit=0
+        self.lastDiffs=[]
+        self.probScore=0
 
-col1.printnum()
-	
+fileData=[]
+lineData=[]
+drawAndDate=[]
+charLineData=[]
+#intLineData=[] Commented out 12/4
+unSortedHits={}
+slp=matrix()
+matrixLength=0
+inFile=open("allnumbers.txt")
+rawFileData=reversed(inFile.readlines())
+for rawLineData in rawFileData:
+    charLineData=rawLineData.split('          ') #split number fields
+    drawAndDate=charLineData[0].split('     ')   #split Draw number and date 
+    drawNumber=int(drawAndDate[0])
+    matrixLength+=1
+    for x in range(1,7):
+        slp.col[x].ball[int(charLineData[x])].hitMatrix.append(drawNumber) #append number to balls hitMatrix
+        slp.col[x].ball[int(charLineData[x])].hits += 1 #add 1 to balls hitlist
+        slp.col[x].ball[int(charLineData[x])].lastHit=drawNumber
+slp.lastDraw=drawNumber
+
+        
